509b664c622b1ca6e6e0ae733856e7bdeeb520f9
DrDreadful <124979607+DrDreadful5Official@users.noreply.github.com>
2024-04-21 12:58:22 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ function handleMouseMove(event) {
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
     
-    const rotateX = (mouseY / rect.height - 0.5) * 15; // Adjust the multiplier for desired rotation sensitivity
-    const rotateY = (mouseX / rect.width - 0.5) * -15; // Adjust the multiplier for desired rotation sensitivity
+    const rotateX = (mouseY / rect.height - 0.5) * 3; // Adjust the multiplier for desired rotation sensitivity
+    const rotateY = (mouseX / rect.width - 0.5) * -3; // Adjust the multiplier for desired rotation sensitivity
     
     wrapper.style.transition = 'transform 0.1s ease-in-out';
     wrapper.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
@@ -19,7 +19,7 @@ function handleMouseMove(event) {
 }
 
 function resetTransform() {
-    wrapper.style.transition = 'transform 0.5s ease-in-out';
+    wrapper.style.transition = 'transform 0.3s ease-in-out';
     wrapper.style.transform = 'rotateX(0deg) rotateY(0deg)';
 }
 
